b8473928ee2a71c4536c21822e23e8f8f5b6e64b
Alfredo Mejia-Narvaez <mejia@cida.gob.ve>
2015-11-19 11:29:52 -0430

    upgrading paper style

--- paper_style.py
+++ paper_style.py
@@ -1,16 +1,17 @@
 from matplotlib import rc
 
-font = {"family":"sans-serif", "sans-serif":"Bitstream Vera Sans", "size":14.0}
-text = {"usetex":False, "latex.preamble":r"\usepackage{amsmath},\usepackage{helvet},\renewcommand{\familydefault}{\sfdefault},\boldmath"}
+font = {"family":"serif", "serif":"Times New Roman", "size":9.0}
+text = {"usetex":True, "latex.preamble":r"\usepackage{amsmath},\renewcommand{\familydefault}{\rmdefault},\renewcommand{\seriesdefault}{\bfseries},\boldmath"}
 
-rc("axes", linewidth=0.5, labelsize="medium", titlesize="medium", labelweight="bold", color_cycle="AFD65C D69D5C 5C8ED6 D6CE5C D67C5C 5CD6B5 D65CC1".split())
-rc("xtick.major", width=0.5)
-rc("xtick", labelsize="medium")
-rc("ytick.major", width=0.5)
-rc("ytick", labelsize="medium")
-rc("legend", numpoints=1, fontsize="medium", frameon=False)
+rc("axes", linewidth=0.5, labelsize=9.0, titlesize=9.0, labelweight="bold", color_cycle="AFD65C D69D5C 5C8ED6 D6CE5C D67C5C 5CD6B5 D65CC1".split())
+rc("xtick.major", width=0.3)
+rc("xtick", labelsize="small")
+rc("ytick.major", width=0.3)
+rc("ytick", labelsize="small")
+rc("legend", numpoints=1, fontsize="xx-small", frameon=False)
 rc("lines", linewidth=1.0, markeredgewidth=0.0, markersize=7)
 rc("patch", linewidth=0.0)
 rc("font", **font)
 rc("text", **text)
 rc("savefig", dpi=92, bbox="tight")
+
