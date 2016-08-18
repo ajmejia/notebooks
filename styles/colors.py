6910f967343929edda47a456f59df2eac123972c
Alfredo Mejia-Narvaez <mejia@cida.gob.ve>
2016-08-18 02:35:23 -0400

    modular styles in RealDealCB13

---
+++ styles/colors.py
@@ -0,0 +1,19 @@
+samples = dict(
+    mock="#333030",
+    m16="#004E66",
+    g05="#FCBE32"
+)
+seds = dict(
+    jpas="#90C987",
+    ugriz="#F6C141",
+    spec="#7BAFDE"
+)
+segr = dict(
+    jpas=[seds["jpas"], "#4EB265"],
+    ugriz=[seds["ugriz"], "#E8601C"],
+    spec=[seds["spec"], "#1965B0"]
+
+)
+greys_map_name = "Greys"
+diver_map_name = "Spectral"
+seque_map_name = "rainbow"
---
+++ styles/labels.py
@@ -0,0 +1,29 @@
+params = dict(
+    M=r"$M_\star$",
+    log_t_M=r"$\left<\log{t_\star}\right>_M$",
+    log_t_L=r"$\left<\log{t_\star}\right>_L$",
+    log_Z_M=r"$\left<\log{Z_\star/Z\odot}\right>_M$",
+    log_Z_L=r"$\left<\log{Z_\star/Z\odot}\right>_L$",
+    Av=r"$A_V$"
+)
+Delta = dict(
+    M=r"$\Delta\log{M_\star}$",
+    log_t_M=r"$\Delta\left<\log{t_\star}\right>_M$",
+    log_t_L=r"$\Delta\left<\log{t_\star}\right>_L$",
+    log_Z_M=r"$\Delta\left<\log{Z_\star}\right>_M$",
+    log_Z_L=r"$\Delta\left<\log{Z_\star}\right>_L$",
+    Av=r"$\Delta A_V$"
+)
+delta = dict(
+    M=r"$\delta\log{M_\star}$",
+    log_t_M=r"$\delta\left<\log{t_\star}\right>_M$",
+    log_t_L=r"$\delta\left<\log{t_\star}\right>_L$",
+    log_Z_M=r"$\delta\left<\log{Z_\star}\right>_M$",
+    log_Z_L=r"$\delta\left<\log{Z_\star}\right>_L$",
+    Av=r"$\delta A_V$"
+)
+delta_g05 = dict(
+    M=r"$\delta_{G05}\log{M_\star}$",
+    log_t_L=r"$\delta_{G05}\left<\log{t_\star}\right>_L$",
+    log_Z_L=r"$\delta_{G05}\left<\log{Z_\star}\right>_L$"
+)
