3796f26f8a00eafa21bba9c2e7c804ed8f5820a1
Alfredo Mejia-Narvaez <mejia@cida.gob.ve>
2016-09-12 23:56:37 -0400

    adding light color

--- styles/colors.py
+++ styles/colors.py
@@ -1,16 +1,17 @@
 samples = dict(
+    light="#D9D4CF",
     dark="#333030",
     cold="#004E66",
     warm="#FCBE32"
 )
 seds = dict(
     jpas="#90C987",
-    ugriz="#F6C141",
+    sdss="#F6C141",
     spec="#7BAFDE"
 )
 segr = dict(
     jpas=[seds["jpas"], "#4EB265"],
-    ugriz=[seds["ugriz"], "#E8601C"],
+    sdss=[seds["sdss"], "#E8601C"],
     spec=[seds["spec"], "#1965B0"]
 
 )
