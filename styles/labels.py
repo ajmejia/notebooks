7307392a77ff801c59909fd0f5a73aeaaca4a48c
Alfredo Mejia-Narvaez <mejia@cida.gob.ve>
2016-11-12 14:38:08 -0400

    adding age units

--- styles/labels.py
+++ styles/labels.py
@@ -1,29 +1,29 @@
 params = dict(
-    M=r"$M_\star$",
-    log_t_M=r"$\left<\log{t_\star}\right>_M$",
-    log_t_L=r"$\left<\log{t_\star}\right>_L$",
-    log_Z_M=r"$\left<\log{Z_\star/Z\odot}\right>_M$",
-    log_Z_L=r"$\left<\log{Z_\star/Z\odot}\right>_L$",
-    Av=r"$A_V$"
+    M=r"\bf $M_\star$",
+    log_t_M=r"\bf $\left<\log{t_\star}/\text{yr}\right>_M$",
+    log_t_L=r"\bf $\left<\log{t_\star}/\text{yr}\right>_L$",
+    log_Z_M=r"\bf $\left<\log{Z_\star/\text{Z}_\odot}\right>_M$",
+    log_Z_L=r"\bf $\left<\log{Z_\star/\text{Z}_\odot}\right>_L$",
+    Av=r"\bf $A_V$"
 )
 Delta = dict(
-    M=r"$\Delta\log{M_\star}$",
-    log_t_M=r"$\Delta\left<\log{t_\star}\right>_M$",
-    log_t_L=r"$\Delta\left<\log{t_\star}\right>_L$",
-    log_Z_M=r"$\Delta\left<\log{Z_\star}\right>_M$",
-    log_Z_L=r"$\Delta\left<\log{Z_\star}\right>_L$",
-    Av=r"$\Delta A_V$"
+    M=r"\bf $\Delta\log{M_\star}$",
+    log_t_M=r"\bf $\Delta\left<\log{t_\star}\right>_M$",
+    log_t_L=r"\bf $\Delta\left<\log{t_\star}\right>_L$",
+    log_Z_M=r"\bf $\Delta\left<\log{Z_\star}\right>_M$",
+    log_Z_L=r"\bf $\Delta\left<\log{Z_\star}\right>_L$",
+    Av=r"\bf $\Delta A_V$"
 )
 delta = dict(
-    M=r"$\delta\log{M_\star}$",
-    log_t_M=r"$\delta\left<\log{t_\star}\right>_M$",
-    log_t_L=r"$\delta\left<\log{t_\star}\right>_L$",
-    log_Z_M=r"$\delta\left<\log{Z_\star}\right>_M$",
-    log_Z_L=r"$\delta\left<\log{Z_\star}\right>_L$",
-    Av=r"$\delta A_V$"
+    M=r"\bf $\delta\log{M_\star}$",
+    log_t_M=r"\bf $\delta\left<\log{t_\star}\right>_M$",
+    log_t_L=r"\bf $\delta\left<\log{t_\star}\right>_L$",
+    log_Z_M=r"\bf $\delta\left<\log{Z_\star}\right>_M$",
+    log_Z_L=r"\bf $\delta\left<\log{Z_\star}\right>_L$",
+    Av=r"\bf $\delta A_V$"
 )
 delta_g05 = dict(
-    M=r"$\delta_{G05}\log{M_\star}$",
-    log_t_L=r"$\delta_{G05}\left<\log{t_\star}\right>_L$",
-    log_Z_L=r"$\delta_{G05}\left<\log{Z_\star}\right>_L$"
+    M=r"\bf $\delta_{G05}\log{M_\star}$",
+    log_t_L=r"\bf $\delta_{G05}\left<\log{t_\star}\right>_L$",
+    log_Z_L=r"\bf $\delta_{G05}\left<\log{Z_\star}\right>_L$"
 )
