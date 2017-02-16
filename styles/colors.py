452aed51fa8637610bde6b5a954fd66027466a8c
Alfredo Mejia-Narvaez <mejia@cida.gob.ve>
2017-02-15 21:30:03 -0400

    updated styles

--- styles/colors.py
+++ styles/colors.py
@@ -5,10 +5,16 @@ samples = dict(
     warm="#FCBE32",
     hot="#BD1550"
 )
+
+#some color schema
+# #7200DA #F9C00C #F9320C
+# #A5D296 #8283A7 #404146
+# #FFBC42 #8F2D56 #218380
+
 seds = dict(
-    spec="#BD1550",
-    jpas="#E97F02",
-    sdss="#F8CA00"
+    spec="#404146",
+    jpas="#8283A7",
+    sdss="#A5D296"
 )
 greys_map_name = "Greys"
 diver_map_name = "Spectral"
