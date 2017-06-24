771d1f1169de9a694fedf3b08374a484f0888123
Alfredo Mejia-Narvaez <mejia@cida.gob.ve>
2017-06-24 19:04:17 -0400

    updating colour styles

--- styles/colors.py
+++ styles/colors.py
@@ -1,10 +1,10 @@
 samples = dict(
     light="#D9D4CF",
     medium="#A3A1A1",
-    dark="#333030",
-    cold="#004E66",
-    warm="#FCBE32",
-    hot="#BD1550"
+    dark="#231F20",#"#333030",
+    cold="#56B4E9",#"#004E66",
+    warm="#009E73",#"#FCBE32",
+    hot="#D55E00",#"#BD1550"
 )
 
 #some color schema
@@ -13,10 +13,10 @@ samples = dict(
 # #FFBC42 #8F2D56 #218380
 
 seds = dict(
-    spec="#404146",
-    jpas="#8283A7",
-    sdss="#A5D296"
+    spec="#56B4E9",#"#404146",
+    jpas="#0072B2",#"#8283A7",
+    sdss="#009E73",#"#A5D296"
 )
-greys_map_name = "Greys"
-diver_map_name = "Spectral"
+greys_map_name = "gist_yarg"
+diver_map_name = "RdGy"
 seque_map_name = "rainbow"
