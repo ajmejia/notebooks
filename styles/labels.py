d8f4aa4525c9cfc6e86a5fa8d5ffc2727106129b
Alfredo Mejia-Narvaez <mejia@cida.gob.ve>
2017-07-03 02:23:44 -0400

    updating styles & labels

--- styles/labels.py
+++ styles/labels.py
@@ -1,30 +1,44 @@
 params = dict(
-    M=r"\bf $\log{M_\star/\text{M}_\odot}$",
-    log_t_M=r"\bf $\left<\log{t_\star}/\text{yr}\right>_M$",
-    log_t_L=r"\bf $\left<\log{t_\star}/\text{yr}\right>_L$",
-    log_Z_M=r"\bf $\left<\log{Z_\star/\text{Z}_\odot}\right>_M$",
-    log_Z_L=r"\bf $\left<\log{Z_\star/\text{Z}_\odot}\right>_L$",
-    Av=r"\bf $A_V$"
+    M=r"$\log{M_\star/\text{M}_\odot}$",
+    log_t_M=r"$\left<\log{t_\star}/\text{a\~no}\right>_M$",
+    log_t_L=r"$\left<\log{t_\star}/\text{a\~no}\right>_L$",
+    log_Z_M=r"$\left<\log{Z_\star/\text{Z}_\odot}\right>_M$",
+    log_Z_L=r"$\left<\log{Z_\star/\text{Z}_\odot}\right>_L$",
+    Av=r"$A_V$"
 )
 Delta = dict(
-    M=r"\bf $\Delta\log{M_\star}$",
-    log_t_M=r"\bf $\Delta\left<\log{t_\star}\right>_M$",
-    log_t_L=r"\bf $\Delta\left<\log{t_\star}\right>_L$",
-    log_Z_M=r"\bf $\Delta\left<\log{Z_\star}\right>_M$",
-    log_Z_L=r"\bf $\Delta\left<\log{Z_\star}\right>_L$",
-    Av=r"\bf $\Delta A_V$"
+    M=r"$\Delta\log{M_\star}$",
+    log_t_M=r"$\Delta\left<\log{t_\star}\right>_M$",
+    log_t_L=r"$\Delta\left<\log{t_\star}\right>_L$",
+    log_Z_M=r"$\Delta\left<\log{Z_\star}\right>_M$",
+    log_Z_L=r"$\Delta\left<\log{Z_\star}\right>_L$",
+    Av=r"$\Delta A_V$"
 )
 Delta_int = dict((kw, Delta[kw].replace("Delta", r"Delta_\text{int}")) for kw in Delta)
 delta = dict(
-    M=r"\bf $\delta\log{M_\star}$",
-    log_t_M=r"\bf $\delta\left<\log{t_\star}\right>_M$",
-    log_t_L=r"\bf $\delta\left<\log{t_\star}\right>_L$",
-    log_Z_M=r"\bf $\delta\left<\log{Z_\star}\right>_M$",
-    log_Z_L=r"\bf $\delta\left<\log{Z_\star}\right>_L$",
-    Av=r"\bf $\delta A_V$"
+    M=r"$\delta\log{M_\star}$",
+    log_t_M=r"$\delta\left<\log{t_\star}\right>_M$",
+    log_t_L=r"$\delta\left<\log{t_\star}\right>_L$",
+    log_Z_M=r"$\delta\left<\log{Z_\star}\right>_M$",
+    log_Z_L=r"$\delta\left<\log{Z_\star}\right>_L$",
+    Av=r"$\delta A_V$"
 )
 delta_g05 = dict(
-    M=r"\bf $\delta_{G05}\log{M_\star}$",
-    log_t_L=r"\bf $\delta_{G05}\left<\log{t_\star}\right>_L$",
-    log_Z_L=r"\bf $\delta_{G05}\left<\log{Z_\star}\right>_L$"
+    M=r"$\delta_\text{G05}\log{M_\star}$",
+    log_t_L=r"$\delta_\text{G05}\left<\log{t_\star}\right>_L$",
+    log_Z_L=r"$\delta_\text{G05}\left<\log{Z_\star}\right>_L$"
 )
+
+ssag_lb = r"SSAG"
+sdss_lb = r"RB"
+jpas_lb = r"RM"
+spec_lb = r"RA"
+
+jpaso_lb = r"RM$^*$"
+speco_lb = r"RA$^*$"
+
+sfgs_lb = r"GFEs"
+pags_lb = r"GPas"
+algs_lb = r"TGal"
+
+tsize = "x-small"
