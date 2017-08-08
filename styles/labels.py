04a2f1dd2e59b0ba3cfa2a36c6ab5cd27bdbfff2
Alfredo Mejia-Narvaez <mejia@cida.gob.ve>
2017-08-08 17:45:39 -0400

    updating styles to match the MNRAS

--- styles/labels.py
+++ styles/labels.py
@@ -1,30 +1,30 @@
 params = dict(
-    M=r"\bf $\log{M_\star/\text{M}_\odot}$",
-    log_t_M=r"\bf $\left<\log{t_\star}/\text{yr}\right>_M$",
-    log_t_L=r"\bf $\left<\log{t_\star}/\text{yr}\right>_L$",
-    log_Z_M=r"\bf $\left<\log{Z_\star/\text{Z}_\odot}\right>_M$",
-    log_Z_L=r"\bf $\left<\log{Z_\star/\text{Z}_\odot}\right>_L$",
+    M=r"\bf $\log{M_*/\text{M}_\odot}$",
+    log_t_M=r"\bf $\left<\log{t_*}/\text{yr}\right>_M$",
+    log_t_L=r"\bf $\left<\log{t_*}/\text{yr}\right>_L$",
+    log_Z_M=r"\bf $\left<\log{Z_*/\text{Z}_\odot}\right>_M$",
+    log_Z_L=r"\bf $\left<\log{Z_*/\text{Z}_\odot}\right>_L$",
     Av=r"\bf $A_V$"
 )
 Delta = dict(
-    M=r"\bf $\Delta\log{M_\star}$",
-    log_t_M=r"\bf $\Delta\left<\log{t_\star}\right>_M$",
-    log_t_L=r"\bf $\Delta\left<\log{t_\star}\right>_L$",
-    log_Z_M=r"\bf $\Delta\left<\log{Z_\star}\right>_M$",
-    log_Z_L=r"\bf $\Delta\left<\log{Z_\star}\right>_L$",
+    M=r"\bf $\Delta\log{M_*}$",
+    log_t_M=r"\bf $\Delta\left<\log{t_*}\right>_M$",
+    log_t_L=r"\bf $\Delta\left<\log{t_*}\right>_L$",
+    log_Z_M=r"\bf $\Delta\left<\log{Z_*}\right>_M$",
+    log_Z_L=r"\bf $\Delta\left<\log{Z_*}\right>_L$",
     Av=r"\bf $\Delta A_V$"
 )
 Delta_int = dict((kw, Delta[kw].replace("Delta", r"Delta_\text{int}")) for kw in Delta)
 delta = dict(
-    M=r"\bf $\delta\log{M_\star}$",
-    log_t_M=r"\bf $\delta\left<\log{t_\star}\right>_M$",
-    log_t_L=r"\bf $\delta\left<\log{t_\star}\right>_L$",
-    log_Z_M=r"\bf $\delta\left<\log{Z_\star}\right>_M$",
-    log_Z_L=r"\bf $\delta\left<\log{Z_\star}\right>_L$",
+    M=r"\bf $\delta\log{M_*}$",
+    log_t_M=r"\bf $\delta\left<\log{t_*}\right>_M$",
+    log_t_L=r"\bf $\delta\left<\log{t_*}\right>_L$",
+    log_Z_M=r"\bf $\delta\left<\log{Z_*}\right>_M$",
+    log_Z_L=r"\bf $\delta\left<\log{Z_*}\right>_L$",
     Av=r"\bf $\delta A_V$"
 )
 delta_g05 = dict(
-    M=r"\bf $\delta_{G05}\log{M_\star}$",
-    log_t_L=r"\bf $\delta_{G05}\left<\log{t_\star}\right>_L$",
-    log_Z_L=r"\bf $\delta_{G05}\left<\log{Z_\star}\right>_L$"
+    M=r"\bf $\delta_{G05}\log{M_*}$",
+    log_t_L=r"\bf $\delta_{G05}\left<\log{t_*}\right>_L$",
+    log_Z_L=r"\bf $\delta_{G05}\left<\log{Z_*}\right>_L$"
 )
--- styles/paper.py
+++ styles/paper.py
@@ -2,18 +2,20 @@ from matplotlib import rc
 from cycler import cycler
 
 clist  = "#114477 #117755 #E8601C #771111 #771144 #4477AA #44AA88 #F1932D #AA4477 #774411 #777711 #AA4455".split()
-font   = {"family":"sans-serif", "size":9.0, "weight":700}
+font   = {"family":"sans-serif", "size":8.0, "weight":400}
 text   = {"usetex":True, "latex.preamble":r"\usepackage{amsmath},\usepackage[helvet]{sfmath},\usepackage{helvet},\renewcommand{\familydefault}{\sfdefault},\boldmath", "hinting":"native"}
 
 rc("figure", figsize=(3.3, 3.3))
 rc("font", **font)
 rc("text", **text)
-rc("axes", linewidth=0.5, labelsize="medium", titlesize="medium", prop_cycle=cycler("color", clist))
+rc("axes", linewidth=0.3, labelsize="medium", titlesize="medium", prop_cycle=cycler("color", clist))
+rc("grid", linewidth=0.3)
 rc("xtick.major", size=3.0, width=0.3, pad=2)
 rc("xtick", labelsize="x-small")
 rc("ytick.major", size=3.0, width=0.3, pad=2)
 rc("ytick", labelsize="x-small")
 rc("lines", linewidth=1.0, markeredgewidth=0.0, markersize=7)
 rc("patch", linewidth=1.0)
+rc("hatch", linewidth=0.5)
 rc("legend", numpoints=1, fontsize="xx-small", frameon=False)
 rc("savefig", format="pdf", dpi=92, bbox="tight")
